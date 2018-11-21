20a4626425322f60abce6156d68a82a54c701f65
cyr <cyrchen@qq.com>
2018-11-21 20:09:40 +0800

    首页公共部分完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -11,7 +11,39 @@ $(function () {
         setTimeout(function () {
             NProgress.done();
         }, 1000)
+    });
+
+
+    // 1.左侧二级切换功能
+    $("#category").click(function () {
+        $(this).next().stop().slideToggle();
     })
+
+    // 2.左侧菜单切换功能
+    $(".lt_topbar .icon_left").click(function () {
+        $(".lt_aside").toggleClass("hidemenu");
+        $(".lt_topbar").toggleClass("hidemenu");
+        $(".lt_main").toggleClass("hidemenu");
+    })
+
+    // 3.公共退出功能
+    $(".lt_topbar .icon_right").click(function () {
+        $("#logoutModal").modal("show");
+    });
+    $("#logoutBtn").click(function () {
+        $.ajax({
+            type: "get",
+            url: "/employee/employeeLogout",
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                if (info.success) {
+                    location.href = "login.html";
+                }
+            }
+        })
+    })
+
 
 
 
---
+++ public/back/js/isLogin.js
@@ -0,0 +1,16 @@
+// 功能需求: 登录拦截, 对于未登录的用户, 拦截到登录页
+
+$.ajax({
+    type: "get",
+    url: "/employee/checkRootLogin",
+    dataType: "json",
+    success: function (info) {
+        console.log(info);
+        if (info.success) {
+            console.log("该用户已登录");
+        }
+        if (info.error === 400) {
+            location.href = "login.html";
+        }
+    }
+})
