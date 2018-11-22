0ed149b670d9835986de10a1d862c0a5ac374e98
cyr <cyrchen@qq.com>
2018-11-22 20:03:14 +0800

    一级分类页面完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -1,50 +1,50 @@
-$(function () {
-    //开启进度条
-    // NProgress.start();
-    // 关闭进度条
-    // NProgress.done();
+  //开启进度条
+  // NProgress.start();
+  // 关闭进度条
+  // NProgress.done();
 
-    $(document).ajaxStart(function () {
-        NProgress.start();
-    })
-    $(document).ajaxStop(function () {
-        setTimeout(function () {
-            NProgress.done();
-        }, 1000)
-    });
-
-
-    // 1.左侧二级切换功能
-    $("#category").click(function () {
-        $(this).next().stop().slideToggle();
-    })
-
-    // 2.左侧菜单切换功能
-    $(".lt_topbar .icon_left").click(function () {
-        $(".lt_aside").toggleClass("hidemenu");
-        $(".lt_topbar").toggleClass("hidemenu");
-        $(".lt_main").toggleClass("hidemenu");
-    })
-
-    // 3.公共退出功能
-    $(".lt_topbar .icon_right").click(function () {
-        $("#logoutModal").modal("show");
-    });
-    $("#logoutBtn").click(function () {
-        $.ajax({
-            type: "get",
-            url: "/employee/employeeLogout",
-            dataType: "json",
-            success: function (info) {
-                console.log(info);
-                if (info.success) {
-                    location.href = "login.html";
-                }
-            }
-        })
-    })
+  $(document).ajaxStart(function () {
+      NProgress.start();
+  })
+  $(document).ajaxStop(function () {
+      setTimeout(function () {
+          NProgress.done();
+      }, 1000)
+  });
 
 
+  $(function () {
+      // 1.左侧二级切换功能
+      $("#category").click(function () {
+          $(this).next().stop().slideToggle();
+      })
+
+      // 2.左侧菜单切换功能
+      $(".lt_topbar .icon_left").click(function () {
+          $(".lt_aside").toggleClass("hidemenu");
+          $(".lt_topbar").toggleClass("hidemenu");
+          $(".lt_main").toggleClass("hidemenu");
+      })
+
+      // 3.公共退出功能
+      $(".lt_topbar .icon_right").click(function () {
+          $("#logoutModal").modal("show");
+      });
+      $("#logoutBtn").click(function () {
+          $.ajax({
+              type: "get",
+              url: "/employee/employeeLogout",
+              dataType: "json",
+              success: function (info) {
+                  console.log(info);
+                  if (info.success) {
+                      location.href = "login.html";
+                  }
+              }
+          })
+      })
 
 
-})
+
+
+  })
--- public/back/js/first.js
+++ public/back/js/first.js
@@ -18,7 +18,7 @@ $(function () {
                 var htmlStr = template("firstTpl", info);
                 $('tbody').html(htmlStr);
 
-                // 分页
+                // 1.分页
                 $("#paginator").bootstrapPaginator({
                     bootstrapMajorVersion: 3,
                     currentPage: info.page,
@@ -28,9 +28,53 @@ $(function () {
                         rander();
                     }
                 })
-
             }
         })
     }
+
+    //2.点击添加按钮，显示模态框
+    $("#addBtn").click(function () {
+        $("#addModal").modal("show");
+    });
+
+    //3.表单校验功能
+    $("#form").bootstrapValidator({
+        // 配置小图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok', // 校验成功
+            invalid: 'glyphicon glyphicon-remove', // 校验失败
+            validating: 'glyphicon glyphicon-refresh' // 校验中
+        },
+        fields: {
+            categoryName: {
+                validators: {
+                    notEmpty: {
+                        message: "请输入一级分类名称"
+                    }
+                }
+            }
+        }
+    })
+
+    // 4. 注册表单校验成功事件, 阻止默认的表单提交, 通过 ajax 提交
+    $("#form").on("success.form.bv", function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "post",
+            url: "/category/addTopCategory",
+            data: $("#form").serialize(),
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                if (info.success) {
+                    $("#addModal").modal("hide");
+                    currentPage = 1;
+                    rander();
+                    $("#form").data("bootstrapValidator").resetForm(true);
+                }
+            }
+        })
+    })
+
 
 })
