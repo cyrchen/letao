a898991c1c0d508c573ae08820960ed090d56f9b
cyr <cyrchen@qq.com>
2018-11-23 11:50:12 +0800

    文件上传功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -21,7 +21,7 @@ $(function () {
                 $("#paginator").bootstrapPaginator({
                     bootstrapMajorVersion: 3,
                     currentPage: info.page,
-                    totalPages: Math.ceil(info.total / info.size) ,
+                    totalPages: Math.ceil(info.total / info.size),
                     onPageClicked: function (event, originalEvent, type, page) {
                         currentPage = page;
                         rander();
@@ -30,5 +30,47 @@ $(function () {
             }
         })
     }
+
+
+    //2.点击添加分类，显示模态框
+    $("#addBtn").click(function () {
+        $("#addModal").modal("show");
+        //发送请求
+        $.ajax({
+            type: "get",
+            url: "/category/queryTopCategoryPaging",
+            data: {
+                page: 1,
+                pageSize: 100
+            },
+            dataType: "json",
+            success: function (info) {
+                console.log(info);
+                var htmlStr = template("dropdownTpl", info);
+                $(".dropdown-menu").html(htmlStr);
+            }
+        })
+    })
+
+
+    // 3.给下拉菜单添加选中功能
+    $(".dropdown-menu").on("click", "a", function () {
+        var txt = $(this).text();
+        $("#dropdownText").text(txt);
+
+    });
+
+    // 4.调用fileUpload 方法, 发送文件上传请求
+    $("#fileupload").fileupload({
+        dataType: "json",
+        done: function (e, data) {
+            console.log(data);
+            var result = data.result;
+            var picUrl = result.picAddr;
+            $("#imgBox img").attr("src", picUrl);
+        }
+    })
+
+
 
 })
