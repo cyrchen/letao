73bfdd5c1953bba94d058942536b5c88ae2baf3f
cyr <cyrchen@qq.com>
2018-11-25 15:44:11 +0800

    手机端首页完成

--- public/front/js/common.js
+++ public/front/js/common.js
@@ -3,3 +3,11 @@ mui('.mui-scroll-wrapper').scroll({
     bounce: true,
     indicators: false
 });
+
+
+
+//获得slider插件对象,进行轮播设置
+var gallery = mui('.mui-slider');
+gallery.slider({
+    interval: 4000 //自动轮播周期，若为0则不自动播放，默认为0；
+});
